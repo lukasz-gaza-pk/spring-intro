0c5e68ab5e475b87f313a6c2855d5294844c3bc1
dominikapasternak <52331435+dominikapasternak@users.noreply.github.com>
2020-01-11 17:03:43 +0100

    Commit 3

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -6,13 +6,15 @@ public class MyApp {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
+        
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-        MessageService messageService2 = applicationContext.getBean("messageService", MessageService.class);
-
         System.out.println(messageService.getMessage());
+        System.out.println(messageService.hashCode());
+        
+        MessageService messageService2 = applicationContext.getBean("messageService", MessageService.class);
         System.out.println(messageService2.getMessage());
-
+        System.out.println(messageService.hashCode());
+        
         applicationContext.close();
     }
 }
