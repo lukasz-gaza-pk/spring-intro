e3522670de210d6a34df3e5d737fe03d639b4fda
spelinar <40338388+spelinar@users.noreply.github.com>
2020-01-15 18:39:09 +0100

    spring2

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,17 +1,16 @@
 package com.example;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MyApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
-        MessageService messageService1 = applicationContext.getBean("messageRandomService", MessageService.class);
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(MyConfig.class);
+        MessageService messageService = appContext.getBean(MyNameMessageService.class);
+        MessageService secondmessageService = appContext.getBean(RandomTextMessageService.class);
         System.out.println(messageService.getMessage());
-        System.out.println(messageService1.getMessage());
-        System.out.println("Hash code 0 = " + messageService.hashCode());
-        System.out.println("Hash code 1 = " + messageService1.hashCode());
-        applicationContext.close();
+        System.out.println(secondmessageService.getMessage());
+        System.out.println(secondmessageService.hashCode());
     }
 }
